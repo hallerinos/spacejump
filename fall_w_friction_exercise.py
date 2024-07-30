c60b5ec778b06f5ceb25c3a23dafa5e95201a712
Andreas Haller <halleritis@gmail.com>
2024-07-30 10:42:48 +0200

    .

--- fall_w_friction_exercise.py
+++ fall_w_friction_exercise.py
@@ -1,7 +1,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-from ode_methods import euler
+from ode_methods import euler, rk4
 import pandas as pd
 
 gr = 1.61803398875
@@ -11,12 +11,10 @@ mpl.rcParams['figure.figsize'] = (gr*s, s)
 
 # Constants
 g = -9.81  # Acceleration due to gravity (m/s^2)
-rho = 1.2  # Air density at sea level (kg/m^3)
 m = 118  # Mass of Baumgartner with equipment (kg)
-Cd = 1  # Drag coefficient (dimensionless)
-
+rho = 1.2  # Air density at sea level (kg/m^3)
 A = 0.9  # Approximate frontal area (m^2)
-H = 7000
+Cd = 1  # Drag coefficient (dimensionless)
 
 # Initial conditions
 initial_altitude = 39000  # Initial altitude (m)
@@ -26,40 +24,43 @@ initial_velocity = 0  # Initial velocity (m/s)
 fig, ax = plt.subplots()
 
 # Time array
-dt = 0.1
-t = np.arange(0, 600, dt)
+dt = 2
+for A in np.linspace(0, 8, 10):
+    for H in np.linspace(1000, 10000, 10):
+        t = np.arange(0, 600, dt)
 
-# Arrays to store results
-altitude = np.zeros_like(t)
-velocity = np.zeros_like(t)
+        # Arrays to store results
+        altitude = np.zeros_like(t)
+        velocity = np.zeros_like(t)
 
-# Set initial conditions
-altitude[0] = initial_altitude
-velocity[0] = initial_velocity
+        # Set initial conditions
+        altitude[0] = initial_altitude
+        velocity[0] = initial_velocity
 
-# Simulation loop
-for i in range(1, len(t)):
-    # Calculate air density
-    rho_h = 
-    
-    # Calculate drag force
-    F_drag = 
-    
-    # Calculate acceleration
-    a = 
-    
-    # Update velocity and position using Euler method
-    velocity[i] = integrate_euler(velocity[i-1], a, dt)
-    altitude[i] = integrate_euler(altitude[i-1], velocity[i], dt)
-    
-    # Stop the simulation if we reach the ground
-    if altitude[i] <= 0:
-        altitude[i] = 0
-        break
+        # Simulation loop
+        for i in range(1, len(t)):
+            # Calculate air density
+            rho_h = rho * np.exp(-altitude[i-1] / H)
 
-ax.set_xlabel('$h\\ {\\rm [km]}$')
-ax.set_ylabel('$v\\ {\\rm [m/s]}$')
-ax.plot(altitude/1000, velocity)
+            # Define the force functions
+            F_drag = lambda t,y :
+            F_g = lambda t,y :
+            a_tot = lambda t,y :
+            
+            # Update velocity and position using Euler method
+            velocity[i] = rk4(a_tot, t[i], velocity[i-1], dt)
+            # Define the velocity function
+            v_tot = lambda t,y :
+            altitude[i] = rk4(v_tot, t[i], altitude[i-1], dt)
+            
+            # Stop the simulation if we reach the ground
+            if altitude[i] <= 0:
+                altitude[i] = 0
+                break
+
+        ax.set_xlabel('$h\\ {\\rm [km]}$')
+        ax.set_ylabel('$v\\ {\\rm [m/s]}$')
+        ax.plot(altitude/1000, velocity)
 
 # compare with actual data
 trajectory = pd.read_csv('velocity_data.csv')
